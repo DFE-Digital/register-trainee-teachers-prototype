51e73ebcda3424a2b0709971ee56b410d7fdbfd0
Ralph <ralph.hawkins@digital.education.gov.uk>
2022-02-10 11:04:50 +0000

    Handles dates better
    - accordions titles show month and year
    - page headings are current academic year

--- app/data/years.js
+++ app/data/years.js
@@ -11,7 +11,7 @@ let academicYears = [
 
 let currentAcademicYear = "2021 to 2022"
 
-let defaultCourseYear = "2021"
+let defaultCourseYear = 2021
 // defaultCourseYear = null
 
 // The first year in the range

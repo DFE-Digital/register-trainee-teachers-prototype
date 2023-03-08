941e82d8d4e22935fc6148a21c7e3beb7ef0a1f6
Ed Horsford <mail@edwardhorsford.com>
2023-03-08 16:32:52 +0000

    More work

--- scripts/generate-trainee-problems.js
+++ scripts/generate-trainee-problems.js
@@ -17,30 +17,46 @@ let providers = require('../app/data/accrediting-providers')
 let trainees = require('../app/data/records.json')
 
 // To keep track of which trainees have already been assigned to a problem
-let traineesCache = {
-  duplicate: [],
-  forgotten: []
-}
+let traineesCache = {}
 
 
-let randomDateInPast = () => {
+let randomDateInPast = (max=false, min=false) => {
 
   let returnDate = faker.date.between(
-      moment().subtract(4, 'years'),
-      moment().subtract(6, 'months')
+      moment().subtract(max || 48, 'months'),
+      moment().subtract(min || 6, 'months')
     )
-
   return returnDate
+}
+
+let problemRatios = {
+  duplicate: 0.2,
+  duplicateDraft: 0.0,
+  forgotten: 0.3,
+  deferredForgotten: 0.05
 }
 
 let problemTypes = {
 // templates for problems? function to generate problems?
   duplicate: function () {
-
     return {
       title: "These trainees appear to be duplicates",
       description: "Review these trainees and either withdraw one, or email us with an action to take",
-      traineeCount: 2
+      traineeCount: 2,
+      filterTrainees: function(trainees) {
+        return trainees.filter(trainee => utils.isActiveStatus(trainee))
+      }
+    }
+
+  },
+  duplicateDraft: function () {
+    return {
+      title: "This draft appears to be a duplicate",
+      description: "Review whether this draft is a duplicate and either delete it or withdraw the existing trainee",
+      traineeCount: 2,
+      filterTrainees: function(trainees) {
+        return trainees.filter(trainee => utils.isDraft(trainee))
+      }
     }
 
   },
@@ -50,34 +66,60 @@ let problemTypes = {
       description: `The expected end date of this trainee was <span class="govuk-!-font-weight-bold">${moment(randomDateInPast()).fromNow()}</span>.
       If the trainee is no longer in training you should award or withdraw them. If they are still in training you should
       update the expected end date of their course.`,
-      traineeCount: 1
+      traineeCount: 1,
+      filterTrainees: function(trainees) {
+        return trainees.filter(trainee => utils.isActiveStatus(trainee))
+      }
+    }
+  },
+  deferredForgotten: function() {
+
+    let relativeDate = moment(randomDateInPast(60, 24)).from(moment(), true)
+    return {
+      title: "Confirm the deferral is still correct",
+      description: `This trainee has been deferred for <span class="govuk-!-font-weight-bold">${relativeDate}</span>.
+      If the trianee is still deferred, please update their expected end date. If they have left the course you should withdraw them.`,
+      traineeCount: 1,
+      filterTrainees: function(trainees) {
+        return trainees.filter(trainee => utils.isDeferred(trainee))
+      }
     }
   }
 }
 
 
 // Create a single user
-const generateTraineeProblem = (provider, activeTrainees) => {
+const generateTraineeProblem = (provider, providerTrainees) => {
 
   if (!provider) console.log("Error in generateProblem: provider missing")
 
-  let randomProblemType = faker.helpers.arrayElement(Object.keys(problemTypes))
+  // let randomProblemType = faker.helpers.arrayElement(Object.keys(problemTypes))
+
+  let randomProblemType = weighted.select(problemRatios)
 
   // Generate a problem
   let problem = Object.assign({}, problemTypes[randomProblemType]())
 
+  // Apply a filter so we only have suitable trainees to choose from
+  let filteredTrainees = problem.filterTrainees(providerTrainees)
+
 
   // Pick a random trainee and remove it from the pool of available trainees
   const pickRandomTrainee = () => {
-    let filteredTrainees = activeTrainees.filter(trainee => !traineesCache[randomProblemType].includes(trainee.id))
+    let traineesNotUsed = filteredTrainees.filter(trainee => !traineesCache[randomProblemType]?.includes(trainee.id))
 
-    let randomTrainee = faker.helpers.arrayElement(filteredTrainees)?.id
+    if (traineesNotUsed.length == 0) {
+      console.log(`Error with pickRandomTrainee: ran out of trainees for ${randomProblemType}!`)
+      return false
+    }
+    else {
+      let randomTrainee = faker.helpers.arrayElement(traineesNotUsed)?.id
 
-    // Make a note of which we've used
-    traineesCache[randomProblemType].push(randomTrainee)
-    // Remove this trainee so it doesn't appear again
-    // _.remove(activeTrainees, trainee => trainee.id == randomTrainee)
-    return randomTrainee
+      // Make a note of which we've used
+      traineesCache[randomProblemType] = traineesCache[randomProblemType] || []
+      traineesCache[randomProblemType].push(randomTrainee)
+      return randomTrainee
+    }
   }
 
   // Allow for a problem having multiple trainees
@@ -86,6 +128,9 @@ const generateTraineeProblem = (provider, activeTrainees) => {
   for (var i = 0; i < problem.traineeCount; i++) {
     problem.trainees.push(pickRandomTrainee())
   }
+
+  problem.trainees = problem.trainees.filter(Boolean)
+
 
   problem.type = randomProblemType
   problem.id = faker.datatype.uuid()
@@ -102,14 +147,14 @@ const generateFakeTraineeProblems = () => {
 
     let providerTrainees = trainees.filter(trainee => trainee.provider == provider.name)
 
-    let activeTrainees = providerTrainees.filter( trainee => utils.isActiveStatus(trainee) )
+    // let activeTrainees = providerTrainees.filter( trainee => utils.isActiveStatus(trainee) )
 
-    // Create between 10 to 100 traineeProblems per provider
-    let numberOfTraineeProblemsToCreate = utils.getRandomArbitrary(20, Math.min(activeTrainees.length / 2, 250))
+    // Create random number of problems per provider with some limits
+    let numberOfTraineeProblemsToCreate = utils.getRandomArbitrary(20, Math.min(providerTrainees.length / 3, 250))
 
     Array(numberOfTraineeProblemsToCreate).fill().map((item, index) => {
 
-      let problem = generateTraineeProblem(provider, activeTrainees)
+      let problem = generateTraineeProblem(provider, providerTrainees)
       traineeProblems.push(problem)
     })
 

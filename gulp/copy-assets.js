c9b05a492af4097dba66848cf7cf673bf7273a7c
Simon Whatley <simonwhatley@gmail.com>
2024-07-19 16:45:39 +0100

    Update Sass Gulp tasks

--- gulp/copy-assets.js
+++ gulp/copy-assets.js
@@ -9,7 +9,9 @@ const gulp = require('gulp')
 const config = require('./config.json')
 
 gulp.task('copy-assets', function () {
-  return gulp.src(['!' + config.paths.assets + 'sass{,/**/*}',
-    config.paths.assets + '/**'])
+  return gulp.src([
+    `${config.paths.assets}/**`,
+    `!${config.paths.assets}/sass/**`
+  ])
     .pipe(gulp.dest(config.paths.public))
 })

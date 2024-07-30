a815977ee6671950a625448bfbe293126964282b
Simon Whatley <simonwhatley@gmail.com>
2024-07-30 15:51:42 +0100

    Separate script for each asset type

--- gulp/copy-assets.js
+++ gulp/copy-assets.js
@@ -8,10 +8,25 @@ const gulp = require('gulp')
 
 const config = require('./config.json')
 
-gulp.task('copy-assets', function () {
-  return gulp.src([
-    `${config.paths.assets}/**`,
-    `!${config.paths.assets}/sass/**`
-  ])
-    .pipe(gulp.dest(config.paths.public))
+gulp.task('copy-images', () => {
+  return gulp.src(
+    config.paths.assets + 'images/*.+(png|jpg|jpeg|gif|svg|ico)',
+    { encoding: false }
+  )
+    .pipe(gulp.dest(config.paths.public + 'images/'))
+})
+
+gulp.task('copy-javascripts', () => {
+  return gulp.src(config.paths.assets + 'javascripts/*')
+    .pipe(gulp.dest(config.paths.public + 'javascripts/'))
+})
+
+gulp.task('copy-data', () => {
+  return gulp.src(config.paths.assets + 'data/**')
+    .pipe(gulp.dest(config.paths.public + 'data/'))
+})
+
+gulp.task('copy-downloads', () => {
+  return gulp.src(config.paths.assets + 'downloads/**')
+    .pipe(gulp.dest(config.paths.public + 'downloads/'))
 })

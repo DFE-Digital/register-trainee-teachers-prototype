e73a02f377f80a803c8e45c4ea9b13daa2aecb2d
Simon Whatley <simonwhatley@gmail.com>
2024-12-05 18:18:05 +0000

    Fix JavaScript lint errors

--- scripts/generate-users.js
+++ scripts/generate-users.js
@@ -1,56 +1,53 @@
 // To generate new users:
 // $ node scripts/generate-users.js
 
+const fs = require('fs')
+const path = require('path')
+const { fakerUK: faker } = require('@faker-js/faker')
+const weighted = require('weighted')
+const moment = require('moment')
+const _ = require('lodash')
+const utils = require('../app/lib/utils.js')
+const userFilters = require('../app/filters/users.js').filters
+const permissions = require('../app/data/permissions.js')
 
-const fs            = require('fs')
-const path          = require('path')
-const { fakerUK: faker }     = require('@faker-js/faker')
-const weighted      = require('weighted')
-const moment        = require('moment')
-const _             = require('lodash')
-const utils         = require('../app/lib/utils.js')
-const userFilters   = require('../app/filters/users.js').filters
-const permissions   = require('../app/data/permissions.js')
-
-let providers = {}
+const providers = {}
 
 // Combine the two provider files together
 // todo: should a single file provide these? the same code exists in session data defaults
-providers.accreditingProviders    = require('../app/data/accrediting-providers')
+providers.accreditingProviders = require('../app/data/accrediting-providers')
 providers.leadPartners = require('../app/data/lead-schools')
 providers.all = providers.accreditingProviders.all.concat(providers.leadPartners.selected)
 providers.selected = providers.accreditingProviders.selected.concat(providers.leadPartners.selected)
 
-
 // Create a single user
-const generateUser = (provider, role='team member') => {
+const generateUser = (provider, role = 'team member') => {
+  if (!provider) console.log('Error in generateUser: provider missing')
 
-  if (!provider) console.log("Error in generateUser: provider missing")
-
-  let user = {}
+  const user = {}
 
   user.id = faker.string.uuid()
-  let firstName = faker.person.firstName()
-  let familyName = faker.person.lastName()
+  const firstName = faker.person.firstName()
+  const familyName = faker.person.lastName()
   user.fullName = `${firstName} ${familyName}`
 
-  let emailDomain = userFilters.makeFakeSchoolDomain(provider.name)
+  const emailDomain = userFilters.makeFakeSchoolDomain(provider.name)
   user.email = `${firstName}.${familyName}@${emailDomain}`.toLowerCase()
 
   // Shuffle the possible permissions - used so that we can pick the first n and to get some selected randomly
-  let shuffledPermissions = faker.helpers.shuffle(permissions.allUserPermissions[provider.type])
+  const shuffledPermissions = faker.helpers.shuffle(permissions.allUserPermissions[provider.type])
 
   let userPermissions = []
 
   // Team members have a varied number of permissions
-  if (role == 'team member'){
+  if (role == 'team member') {
     // How many permissions should we have
-    let targetCountOfPermissions = utils.getRandomArbitrary(1, shuffledPermissions.length + 1)
+    const targetCountOfPermissions = utils.getRandomArbitrary(1, shuffledPermissions.length + 1)
     // Assign those permissions
     userPermissions = shuffledPermissions.slice(0, Math.min(shuffledPermissions.length, targetCountOfPermissions))
   }
   // Admins have all permissions
-  else if (role == 'team admin'){
+  else if (role == 'team admin') {
     // Team admins have all permissions plus the ability to manage team members
     userPermissions = ['manage team members', ...shuffledPermissions]
   }
@@ -69,25 +66,23 @@ const generateUser = (provider, role='team member') => {
 }
 
 const generateFakeUsers = () => {
-  let users = []
+  const users = []
 
   providers.selected.forEach(provider => {
-
-    let isAccrediting = provider.type == "accreditingProvider"
+    const isAccrediting = provider.type == 'accreditingProvider'
     // Create between 2 and 10 users per provider
-    let numberOfUsersToCreate = utils.getRandomArbitrary(2, (isAccrediting)? 10 : 3)
+    const numberOfUsersToCreate = utils.getRandomArbitrary(2, (isAccrediting) ? 10 : 3)
 
     Array(numberOfUsersToCreate).fill().map((item, index) => {
       // First user always a team admin. After that, 20% chance.
-      let role = "team admin"
+      let role = 'team admin'
       if (index != 0) {
-        role = weighted.select(['team admin', 'team member'], [0.2,0.8])
+        role = weighted.select(['team admin', 'team member'], [0.2, 0.8])
       }
-      let user = generateUser(provider, role)
+      const user = generateUser(provider, role)
 
       users.push(user)
     })
-
   })
 
   return users

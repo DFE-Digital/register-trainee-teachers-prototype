1cb15867473279b4e3e9a64d901836bed20074fb
Simon Whatley <simonwhatley@gmail.com>
2024-12-18 14:03:27 +0000

    Delete gulp scripts

--- gulp/school-search-index.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp')
-const lunr = require('./../scripts/generate-search-index') // generate
-
-gulp.task('school-search-index', (done) => {
-  // Generate search index
-  lunr()
-
-  done()
-})

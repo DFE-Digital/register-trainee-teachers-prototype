a83e857e678e3acb115b2ccb24a0298a269ea1c9
Andrew Scrivener <andrew.scrivener@education.gov.uk>
2025-03-03 11:24:02 +0000

    Removed token descriptions
    The team decided that a name should be sufficient.
    
    Signed-off-by: Andrew Scrivener <andrew.scrivener@education.gov.uk>

--- app/data/api-tokens.js
+++ app/data/api-tokens.js
@@ -3,19 +3,16 @@
 const generatedTokens = [
   {
     tokenName: 'Token 1',
-    tokenDescription: 'Maecenas at odio odio',
     lastUsedDate: '01/02/2024',
     expirationDate: '01/12/2024'
   },
   {
     tokenName: 'For Ernest Ryan',
-    tokenDescription: 'Nulla dapibus vitae nisl vitae venenatis',
     lastUsedDate: 'Never',
     expirationDate: '01/12/2024'
   },
   {
     tokenName: "Carlie Yundt's",
-    tokenDescription: 'Vestibulum lobortis neque libero.',
     lastUsedDate: '19/02/2024',
     expirationDate: '01/12/2024'
   }

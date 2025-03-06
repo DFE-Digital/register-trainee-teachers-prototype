6579b23395fc72a68f919c855ed6f10820c565d0
Andrew Scrivener <andrew.scrivener@education.gov.uk>
2025-03-06 11:38:54 +0000

    Alterations to example token data
    Signed-off-by: Andrew Scrivener <andrew.scrivener@education.gov.uk>

--- app/data/api-tokens.js
+++ app/data/api-tokens.js
@@ -1,31 +1,46 @@
 // This is to simulate some fake API tokens for testing
 
-const generatedTokens = [{
-    tokenName: 'Token 1',
-    lastUsedDate: '01/02/2024',
-    expirationDate: '01/12/2024'
+const tokens = [{
+   tokenName: 'Bayer Group Vendor Test',
+   tokenStatus: 'Active',
+   createdBy: 'John Doe',
+   dateCreated: '01/12/2024',
+   lastUsedDate: '01/02/2024',
+   dateRevoked: '01/12/2024',
+   dateExpired: '01/12/2024',
+   tokenActions: 'Revoke'
   },
   {
     tokenName: 'For Ernest Ryan',
-    lastUsedDate: 'Never',
-    expirationDate: '01/12/2024'
+    tokenStatus: 'Expired',
+    createdBy: 'John Doe',
+    dateCreated: '01/12/2024',
+    lastUsedDate: '01/02/2024',
+    dateRevoked: '01/12/2024',
+    dateExpired: '01/12/2024',
+    tokenActions: 'Revoke'
   },
   {
-    tokenName: "Carlie Yundt's",
+    tokenName: 'Carlie Yundt\'s',
+    tokenStatus: 'Expired',
+    createdBy: 'John Doe',
+    dateCreated: '01/12/2024',
     lastUsedDate: '19/02/2024',
-    expirationDate: '01/12/2024'
+    dateRevoked: '01/12/2024',
+    dateExpired: '01/12/2024',
+    tokenActions: 'Revoke'
   }
 ]
 
 function generateNewToken() {
   const part1 = 'yweuyi'
   const part2 = 172351273
-  const token = (part1 + part2).toString()
+  const token = `${part1}${part2}`;
   console.log(token)
   return token
 }
 
 module.exports = {
-  generatedTokens,
-  generateNewToken,
+  tokens,
+  generateNewToken
 }

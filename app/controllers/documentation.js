5ebf2eaab3ea3d3f00357d759aeac9f798a55feb
Simon Whatley <simonwhatley@gmail.com>
2025-10-28 14:30:55 +0000

    Add documentation controller

---
+++ app/controllers/documentation.js
@@ -0,0 +1,20 @@
+exports.api_get = async (req, res) => {
+
+  res.render('docs/api/index', {
+
+  })
+}
+
+exports.csv_get = async (req, res) => {
+
+  res.render('docs/csv/index', {
+
+  })
+}
+
+exports.referenceData_get = async (req, res) => {
+
+  res.render('docs/reference-data/index', {
+
+  })
+}

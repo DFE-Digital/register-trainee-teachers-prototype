eac206bd343ba441842f38255c7b264f7b97664c
Simon Whatley <simonwhatley@gmail.com>
2026-01-14 15:53:36 +0000

    Update trainee model

--- app/models/trainee.js
+++ app/models/trainee.js
@@ -4,20 +4,25 @@ const findMany = (params) => {
   return trainees
 }
 
-const findOne = (params) => {
-  let trainee = {}
+const findOne = (params = {}) => {
+  const trainee = trainees.find((item) => {
+    if (params.traineeId && item.id === params.traineeId) {
+      return true
+    }
 
-  if (params.traineeId || params.trn || params.hesaId) {
-    trainee = trainees.find(trainee =>
-      trainee.id === params.traineeId ||
-      trainee.trn === params.trn ||
-      trainee.hesaId === params.hesaId
-    )
-  }
+    if (params.trn && String(item.trainingDetails?.trn) === String(params.trn)) {
+      return true
+    }
 
-  return trainee
+    if (params.hesaId && String(item.trainingDetails?.hesaId) === String(params.hesaId)) {
+      return true
+    }
+
+    return false
+  })
+
+  return trainee || {}
 }
-
 
 module.exports = {
   findMany,

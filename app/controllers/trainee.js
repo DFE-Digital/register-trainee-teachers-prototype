b6ab7b24676142da31b4da2d2163184da5381e34
Simon Whatley <simonwhatley@gmail.com>
2026-01-14 13:49:09 +0000

    Use trainee model

--- app/controllers/trainee.js
+++ app/controllers/trainee.js
@@ -1,10 +1,29 @@
+const { findOne } = require('../models/trainee')
+
 exports.show = async (req, res) => {
   delete req.session.data.withdrawal
   delete req.session.data.referrer
 
   const { traineeId } = req.params
+  const trainee = findOne({ traineeId })
 
   res.render('trainees/index', {
+    trainee,
+    actions: {
+      back: '#',
+      defer: `/trainees/${traineeId}/defer`,
+      withdraw: `/trainees/${traineeId}/withdraw`,
+      outcome: `/trainees/${traineeId}/outcome/when`
+    }
+  })
+}
+
+exports.personal = async (req, res) => {
+  const { traineeId } = req.params
+  const trainee = findOne({ traineeId })
+
+  res.render('trainees/personal', {
+    trainee,
     actions: {
       back: '#',
       defer: `/trainees/${traineeId}/defer`,

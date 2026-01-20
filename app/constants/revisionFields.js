cdcdd28a929d1dcb48a1208538b4622e09b16aef
Simon Whatley <simonwhatley@gmail.com>
2026-01-20 12:08:22 +0000

    Add constants

---
+++ app/constants/revisionFields.js
@@ -0,0 +1,23 @@
+module.exports = {
+  provider: [
+    'operatingName',
+    'legalName',
+    'type',
+    'ukprn',
+    'urn',
+    'code',
+    'website',
+    'archivedAt',
+    'archivedById',
+    'deletedAt',
+    'deletedById'
+  ],
+  user: [
+    'firstName',
+    'lastName',
+    'email',
+    'isActive',
+    'deletedAt',
+    'deletedById'
+  ]
+}
